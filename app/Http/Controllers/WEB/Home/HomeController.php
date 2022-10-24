25f268cb57366e60c7a502ff1af25a261cdddb00
Juan Pablo Gomez <85317633+DevGPH@users.noreply.github.com>
2022-10-24 19:50:24 +0200

    routes home

--- app/Http/Controllers/WEB/Home/HomeController.php
+++ app/Http/Controllers/WEB/Home/HomeController.php
@@ -23,6 +23,105 @@ class HomeController extends Controller
         ]); 
     }
 
+    public function rooms($locale)
+    {
+        $rate = $this->rateToday($locale);
+        return view('storefront.rooms')->with([
+            'room_active' => 'active-link',
+            'lang' =>(App::getLocale() == 'es') ? 'en' : 'es',
+            'rate' => $rate
+        ]); 
+    }
+
+    public function contact($locale)
+    {
+        $rate = $this->rateToday($locale);
+        return view('storefront.contact')->with([
+            'contact_active' => 'active-link',
+            'lang' =>(App::getLocale() == 'es') ? 'en' : 'es',
+            'rate' => $rate
+        ]); 
+    }
+
+    public function covid($locale)
+    {
+        $rate = $this->rateToday($locale);
+        return view('storefront.covid')->with([
+            'covid_active' => 'active-link',
+            'lang' =>(App::getLocale() == 'es') ? 'en' : 'es',
+            'rate' => $rate
+        ]); 
+    }
+
+    public function menu($locale)
+    {
+        $rate = $this->rateToday($locale);
+        return view('storefront.menu')->with([
+            'lang' =>(App::getLocale() == 'es') ? 'en' : 'es',
+            'rate' => $rate
+        ]);    
+    }
+
+    public function roomService($locale)
+    {
+        $rate = $this->rateToday($locale);
+        return view('storefront.room_service')->with([
+            'lang' =>(App::getLocale() == 'es') ? 'en' : 'es',
+            'rate' => $rate
+        ]);  
+    }
+
+    public function getUserC(Request $request, $locale)
+    {
+        $response = Http::withHeaders([
+            'X-User' => 'clubestrella',
+            'X-Secret' => 'S0port*2020',
+        ])->post('https://clubestrella.mx/api/login-adhara', [
+            'email' => $request->input('email'),
+            'password' => $request->input('password')
+        ]);
+
+        $result = $response->json();
+        if (isset($result['error'])) {
+            return response()->json(['error'=> $result['code']], $result['code']);
+        }
+
+        $countryName = $result['data']['cliente']['pais'];
+        
+        $paises = Pais::where('nombre', $countryName)->first();
+        if ($paises == null) {
+            $paises = Pais::where('id', 145)->first();
+            $countryName = $paises->nombre;
+        }
+
+        $arrayUser =  [
+            'id' => $result['data']['id'],
+            'name' => $result['data']['nombre'],
+            'lastname' => $result['data']['apellidos'],
+            'email'  => $result['data']['email'],
+            'partner_number'  => $result['data']['cliente']['numero_socio'],
+            'city'  => $result['data']['cliente']['ciudad'],
+            'state'  => $result['data']['cliente']['estado'],
+            'country'  => $countryName,
+            'country_id'  => $paises->id,
+            'number'  => $result['data']['cliente']['celular'],
+            'total_points'  => $result['data']['puntos']['puntos_totales']
+        ];
+        $minutes = 30;
+
+        $response = new Response('Set Cookie');
+        $response->withCookie(cookie('user', json_encode($arrayUser), $minutes));
+        
+        return $response;
+
+        // return "session iniciada";
+    }
+    public function closeUserC(Request $request, $locale)
+    {
+        Cookie::queue(Cookie::forget('user'));
+        return redirect('/');
+    }
+
     function rateToday($locale)
     {
         $pointer = now();
