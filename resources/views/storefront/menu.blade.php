ab74c622c4f0cf4a715125aa33236c9374d8d969
Juan Pablo Gomez <85317633+DevGPH@users.noreply.github.com>
2022-10-27 19:53:22 +0200

    menu adhara

--- resources/views/storefront/menu.blade.php
+++ resources/views/storefront/menu.blade.php
@@ -5,12 +5,12 @@
 <div style="margin-top:90px">
     @if ($lang == 'es')
         <img src="{{ asset('images/menus/menu_en.png') }}" alt="Menu del Restaurante" class="img-fluid img-c">
-        <img src="{{ asset('images/menus/menu2_en.png') }}" alt="Menu del Restaurante" class="img-fluid img-c">
-        <img src="{{ asset('images/menus/menu3_en.png') }}" alt="Menu del Restaurante" class="img-fluid img-c">
+        <img src="{{ asset('images/menus/menu_coco_en.png') }}" alt="Menu del Restaurante" class="img-fluid img-c">
+        <img src="{{ asset('images/menus/menu_drinks_en.png') }}" alt="Menu del Restaurante" class="img-fluid img-c">
     @else
-        <img src="{{ asset('images/menus/menu_esp.png') }}" alt="Menu del Restaurante" class="img-fluid img-c">
-        <img src="{{ asset('images/menus/menu2_esp.png') }}" alt="Menu del Restaurante" class="img-fluid img-c">
-        <img src="{{ asset('images/menus/menu3_esp.png') }}" alt="Menu del Restaurante" class="img-fluid img-c">
+        <img src="{{ asset('images/menus/menu_es.png') }}" alt="Menu del Restaurante" class="img-fluid img-c">
+        <img src="{{ asset('images/menus/menu_coco_es.png') }}" alt="Menu del Restaurante" class="img-fluid img-c">
+        <img src="{{ asset('images/menus/menu_drinks_es.png') }}" alt="Menu del Restaurante" class="img-fluid img-c">
     @endif
 </div>
 
--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,7 @@ Route::prefix('{locale}')->group(function ()
     Route::get('/booking',[ReservaController::class,'index'])->name('booking');//se crearon 2 rutas de bookin para resolver el problema de que en el formulario al usar get se busca con la informacion de la consulta anterior 
     Route::post('/booking',[ReservaController::class,'index'])->name('bookingP');// y que  con post al cambiar el idioma se recargaba la pagina y como ya no habia parametros en el requets fallaba  
     Route::get('/accommodations',[AccommodationsController::class,'accommodations'])->name('accommodations');
-    Route::get('/menu_restaurant',[HomeController::class,'menu'])->name('menu.hotel');
+    Route::get('/menu_adhara',[HomeController::class,'menu'])->name('menu.hotel');
 
     Route::get('/reservations',[ReservaController::class,'reservations'])->name('reservations');
     Route::post('/book-reserve',[ReservaController::class,'store'])->name('book.reserve');
