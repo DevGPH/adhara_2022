da85753d8277130416f0a3315566f7fa387b84aa
Juan Pablo Gomez <85317633+DevGPH@users.noreply.github.com>
2022-10-27 19:59:57 +0200

    tipo img

--- resources/views/storefront/room_service.blade.php
+++ resources/views/storefront/room_service.blade.php
@@ -4,10 +4,10 @@
 
 <div style="margin-top: 90px;">
     @if ($lang == 'es')
-        <img src="{{ asset('images/menus/room_service_menu_en.jpg') }}" alt="Menu del Restaurante" class="img-fluid img-c">
+        <img src="{{ asset('images/menus/room_service_menu_en.png') }}" alt="Menu del Restaurante" class="img-fluid img-c">
         <img src="{{ asset('images/menus/room_service_coco_en.jpg') }}" alt="Menu del Restaurante" class="img-fluid img-c">
     @else
-        <img src="{{ asset('images/menus/room_service_menu_es.jpg') }}" alt="Room Service" class="img-fluid img-c">
+        <img src="{{ asset('images/menus/room_service_menu_es.png') }}" alt="Room Service" class="img-fluid img-c">
         <img src="{{ asset('images/menus/room_service_coco_es.jpg') }}" alt="Room Service" class="img-fluid img-c">
     @endif
 </div>
