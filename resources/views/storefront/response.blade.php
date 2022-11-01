53d5adf2528f21c5f4d3e6d61cd4546f77654ee9
Juan Pablo Gomez <85317633+DevGPH@users.noreply.github.com>
2022-11-01 18:20:36 +0100

    response adhara

--- resources/views/storefront/response.blade.php
+++ resources/views/storefront/response.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-<link rel="stylesheet" href="{{ asset('css/web/response.css') }}">
+<link rel="stylesheet" href="{{ asset('css/web/response_adhara.css') }}">
 
-    <div id="existoso" class="container " style="margin-top:100px">
+    <div id="existoso" class="main-body " style="margin-top:100px">
         <div class="row" style="margin-top:0px">
             <div class="col-xs-12 col-sm-12 col-md-12">
                 <img src="{{ asset('images/icons/reserve.png') }}" alt="" id="response">
