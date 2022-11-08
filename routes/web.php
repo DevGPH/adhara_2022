ee5d4e6af1c78c40cff58082b158486d2c98a2f8
Juan Pablo Gomez <85317633+DevGPH@users.noreply.github.com>
2022-11-08 19:01:30 +0100

    santander controller missing

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\WEB\Home\HomeController;
 use App\Http\Controllers\WEB\Reservas\ReservaController;
+use App\Http\Controllers\WEB\Santander\SantanderController;
 
 /*
 |--------------------------------------------------------------------------
