edd6722f83f0c77475292041ab586b5fa3b43f55
Juan Pablo Gomez <85317633+DevGPH@users.noreply.github.com>
2022-11-17 19:31:42 +0100

    new menu cocodrillos

--- resources/views/storefront/menu.blade.php
+++ resources/views/storefront/menu.blade.php
@@ -5,11 +5,11 @@
 <div style="margin-top:90px">
     @if ($lang == 'es')
         <img src="{{ asset('images/menus/menu_en.png') }}" alt="Menu del Restaurante" class="img-fluid img-c">
-        <img src="{{ asset('images/menus/menu_coco_en.png') }}" alt="Menu del Restaurante" class="img-fluid img-c">
+        <img src="{{ asset('images/menus/menu_cocodrillos_en.png') }}" alt="Menu del Restaurante" class="img-fluid img-c">
         <img src="{{ asset('images/menus/menu_drinks_en.png') }}" alt="Menu del Restaurante" class="img-fluid img-c">
     @else
         <img src="{{ asset('images/menus/menu_es.png') }}" alt="Menu del Restaurante" class="img-fluid img-c">
-        <img src="{{ asset('images/menus/menu_coco_es.png') }}" alt="Menu del Restaurante" class="img-fluid img-c">
+        <img src="{{ asset('images/menus/menu_cocodrillos_es.png') }}" alt="Menu del Restaurante" class="img-fluid img-c">
         <img src="{{ asset('images/menus/menu_drinks_es.png') }}" alt="Menu del Restaurante" class="img-fluid img-c">
     @endif
 </div>
