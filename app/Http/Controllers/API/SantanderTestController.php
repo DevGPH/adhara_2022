e711012c6b3e4f78fd2e3e5b3a494bd3a672e061
Juan Pablo Gomez <85317633+DevGPH@users.noreply.github.com>
2022-11-22 19:53:59 +0100

    xml debug element

--- app/Http/Controllers/API/SantanderTestController.php
+++ app/Http/Controllers/API/SantanderTestController.php
@@ -138,7 +138,7 @@ class SantanderTestController extends Controller
                 $satander_response->url_mitec = $sxe->nb_url;
                 $satander_response->huesped_id = $huesped->id;
                 $satander_response->save();
-                dd($sxe->nb_url);
+                dd($sxe, $descrypted_xml);
                 if( strcmp( $sxe->cd_response, "success") == 0 )
                 {
                     //header("Location: ".$sxe->nb_url);
