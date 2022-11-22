a129e6de80132f8c70d79f93e579f5bafeb5312f
Juan Pablo Gomez <85317633+DevGPH@users.noreply.github.com>
2022-11-22 19:50:00 +0100

    test folio

--- routes/api.php
+++ routes/api.php
@@ -22,3 +22,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 
 Route::get('/santander-keys',[SantanderTestController::class,'keys']);
+Route::get('/santander-folio/{folio}',[SantanderTestController::class,'index']);
