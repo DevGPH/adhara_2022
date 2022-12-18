2080e20513fe4bd3101e0d12aca9f9539b1433d8
Juan Pablo Gomez <85317633+DevGPH@users.noreply.github.com>
2022-12-18 14:14:31 +0100

    nuevo menu dec 2022

--- resources/views/storefront/menu.blade.php
+++ resources/views/storefront/menu.blade.php
@@ -4,13 +4,13 @@
 
 <div style="margin-top:90px">
     @if ($lang == 'es')
-        <img src="{{ asset('images/menus/menu_english.png') }}" alt="Menu del Restaurante" class="img-fluid img-c">
-        <img src="{{ asset('images/menus/menu_coco_en.png') }}" alt="Menu del Restaurante" class="img-fluid img-c">
-        <img src="{{ asset('images/menus/menu_bebidas_en.png') }}" alt="Menu del Restaurante" class="img-fluid img-c">
+        <img src="{{ asset('images/menus/menu_en.png') }}" alt="Menu del Restaurante" class="img-fluid img-c">
+        <img src="{{ asset('images/menus/coco_en.png') }}" alt="Menu del Restaurante" class="img-fluid img-c">
+        <img src="{{ asset('images/menus/drinks_en.png') }}" alt="Menu del Restaurante" class="img-fluid img-c">
     @else
-        <img src="{{ asset('images/menus/menu_spanish.png') }}" alt="Menu del Restaurante" class="img-fluid img-c">
-        <img src="{{ asset('images/menus/menu_coco_es.png') }}" alt="Menu del Restaurante" class="img-fluid img-c">
-        <img src="{{ asset('images/menus/menu_bebidas_es.png') }}" alt="Menu del Restaurante" class="img-fluid img-c">
+        <img src="{{ asset('images/menus/menu_es.png') }}" alt="Menu del Restaurante" class="img-fluid img-c">
+        <img src="{{ asset('images/menus/coco_es.png') }}" alt="Menu del Restaurante" class="img-fluid img-c">
+        <img src="{{ asset('images/menus/drinks_es.png') }}" alt="Menu del Restaurante" class="img-fluid img-c">
     @endif
 </div>
 
