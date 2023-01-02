6017172c645a2039bac43b3fd49f640c6e05719a
Juan Pablo Gomez <85317633+DevGPH@users.noreply.github.com>
2023-01-02 21:03:17 +0100

    agregar grill

--- resources/views/storefront/menu.blade.php
+++ resources/views/storefront/menu.blade.php
@@ -4,11 +4,11 @@
 
 <div style="margin-top:90px">
     @if ($lang == 'es')
-        <img src="{{ asset('images/menus/menu_en.png') }}" alt="Menu del Restaurante" class="img-fluid img-c">
+        <img src="{{ asset('images/menus/menu_grill_en.png') }}" alt="Menu del Restaurante" class="img-fluid img-c">
         <img src="{{ asset('images/menus/cocodrillos_en.png') }}" alt="Menu del Restaurante" class="img-fluid img-c">
         <img src="{{ asset('images/menus/bebidas_en.png') }}" alt="Menu del Restaurante" class="img-fluid img-c">
     @else
-        <img src="{{ asset('images/menus/menu_es.png') }}" alt="Menu del Restaurante" class="img-fluid img-c">
+        <img src="{{ asset('images/menus/menu_grill_es.png') }}" alt="Menu del Restaurante" class="img-fluid img-c">
         <img src="{{ asset('images/menus/cocodrillos_es.png') }}" alt="Menu del Restaurante" class="img-fluid img-c">
         <img src="{{ asset('images/menus/bebidas_es.png') }}" alt="Menu del Restaurante" class="img-fluid img-c">
     @endif
