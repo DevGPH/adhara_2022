bdf1411b88987271e85a23a828616a1b11d633d7
Juan Pablo Gomez <85317633+DevGPH@users.noreply.github.com>
2023-04-13 12:29:26 +0200

    footer done

--- resources/views/layouts/web/footer.blade.php
+++ resources/views/layouts/web/footer.blade.php
@@ -5,23 +5,22 @@
         <div class="wrapper">
             <div class="row">
                 <div class="col-lg-6">
-                    <h5>SOCIAL MEDIA</h5>
-                    <h6>FOLLOW THE GRADIUM</h6>
+                    <h5>REDES SOCIALES</h5>
+                    <h6>SIGUENOS EN TU PLATAFORMA FAVORITA</h6>
                     <div class="widget-social-icons">
                         <ul>
                             <li><a href="#" target="_blank"><i class="fa fa-facebook"></i></a></li>
-                            <li><a href="#" target="_blank"><i class="fa fa-twitter"></i></a></li>
-                            <li><a href="#" target="_blank"><i class="fa fa-google-plus"></i></a></li>
+                            <li><a href="#" target="_blank"><i class="fa-brands fa-tiktok"></i></a></li>
                             <li><a href="#" target="_blank"><i class="fa fa-instagram"></i></a></li>
                         </ul>
                     </div>
                 </div>
                 <div class="col-lg-6">
-                    <h5>NEWSLETTER</h5>
-                    <h6>WE LOVE TO SHARE NEW OFFERS AND EXLUSIVE PROMOTIONS</h6>
+                    <h5>NOTICIAS RECIENTES</h5>
+                    <h6>TE ENVIAREMOS NUESTRAS PROMOCIONES MAS RECIENTES</h6>
                     <div class="widget-newsletter">
                         <form>
-                            <input type="text" placeholder="ENTER YOUR E-MAIL ADDRESS" required>
+                            <input type="text" placeholder="ESCRIBE TU E-MAIL" required>
                             <button type="submit"><i class="fa fa-paper-plane"></i></button>
                         </form>
                     </div>
@@ -37,20 +36,33 @@
                 <img src="{{ asset('images/logos/logo_adhara.png') }}" alt="The Grandium Hotel">
             </div>
             <div class="footer-copyright">
-                <p>COPYRIGHT © THE GRANDIUM HOTEL</p>
+                <p>HOTEL ADHARA CANCÚN PERTENECE A GRUPO PENINSULAR DE HOTELES</p>
             </div>
+            <div id="gph" style="margin-top:40px;">
+                <img src="{{ asset('images/footer/gph.png') }}" alt="Grupo Peninsular de Hoteles" style="width:150px;display:block;margin:0px auto;">
+            </div>
+            <ul class="client-footers">
+                <li><img src="{{ asset('images/footer/express.png') }}" alt=""></li>
+                <li><img src="{{ asset('images/footer/clubestrella.png') }}" alt=""></li>
+                <li><img src="{{ asset('images/footer/oktrip.png') }}" alt=""></li>
+                <li><img src="{{ asset('images/footer/grill.png') }}" alt=""></li>
+                <li><img src="{{ asset('images/footer/cocodrillos.png') }}" alt=""></li>
+                <li><img src="{{ asset('images/footer/eventos.png') }}" alt=""></li>
+            </ul>
             <div class="footer-contact">
                 <ul>
-                    <li><i class="fa fa-map-marker"></i> LONGRIDGE ROAD, EARLS COURT, LONDON</li>
-                    <li><i class="fa fa-phone"></i> +1-800-123-45-67</li>
-                    <li><a href="mailto:info@thegrandiumhotel.com"><i class="fa fa-paper-plane"></i> INFO@THEGRANDIUMHOTEL.COM</a></li>
+                    <li><img src="{{ asset('images/footer/pagoseguro.png') }}" alt="" style="width: 120px;"></li>
+                    <li><img src="{{ asset('images/footer/ssl.png') }}" alt="" style="width: 120px;"></li>
+                    <li><img src="{{ asset('images/footer/hacker.png') }}" alt="" style="width: 120px;"></li>
+                    <li><img src="{{ asset('images/footer/santander.png') }}" alt="" style="width: 120px;"></li>
+                    <li><img src="{{ asset('images/footer/mexico.png') }}" alt="" style="width: 120px;"></li>
                 </ul>
             </div>
             <div class="footer-nav">
                 <ul>
-                    <li><a href="#">SITEMAP</a></li>
-                    <li><a href="#">PRIVACY POLICY</a></li>
-                    <li><a href="#">TERMS OF USE</a></li>
+                    <!--li><a href="#">SITEMAP</a></li-->
+                    <li><a href="#">AVISO DE PRIVACIDAD</a></li>
+                    <li><a href="#">TERMINOS Y CONDICIONES</a></li>
                 </ul>
             </div>
         </div>
