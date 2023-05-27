0a58cf098d1f4fc2ff458c85ace3dd5b30132c54
Juan Pablo Gomez <85317633+DevGPH@users.noreply.github.com>
2023-05-27 13:04:46 +0200

    styles jquery

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
 	<link rel="stylesheet" type="text/css" href="{{ asset('css/magnific.popup.css') }}">
 	<link rel="stylesheet" type="text/css" href="{{ asset('css/owl.carousel.css') }}">
 	<link rel="stylesheet" type="text/css" href="{{ asset('css/owl.theme.default.css') }}">
-	<link rel="stylesheet" type="text/css" href="{{ asset('css/jquery-ui.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('css/jquery-uit.css') }}">
 	<link rel="stylesheet" type="text/css" href="{{ asset('css/queen_osi.css') }}">
 	<link rel="stylesheet" type="text/css" href="{{ asset('css/mobile_master.css') }}">
 	<link rel="stylesheet" type="text/css" href="{{ asset('css/homev2.css') }}">
