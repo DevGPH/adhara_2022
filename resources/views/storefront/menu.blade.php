21708d75e43f969a8146a45c43ac4cbb1aac7ad2
Juan Pablo Gomez <85317633+DevGPH@users.noreply.github.com>
2023-06-08 19:57:39 +0200

    nuevo menu

--- resources/views/storefront/menu.blade.php
+++ resources/views/storefront/menu.blade.php
@@ -4,10 +4,12 @@
 
 <div class="site-main">
     @if ($lang == 'es')
+        <img src="{{ asset('images/menus/menunuevo_en.png') }}" alt="Menu del Restaurante" style="display:block;margin:0px auto;">
         <img src="{{ asset('images/menus/grill_en.png') }}" alt="Menu del Restaurante" style="display:block;margin:0px auto;">
         <img src="{{ asset('images/menus/cocodrillos_en.png') }}" alt="Menu del Restaurante" style="display:block;margin:0px auto;">
         <img src="{{ asset('images/menus/bebidas_en.png') }}" alt="Menu del Restaurante" style="display:block;margin:0px auto;">
     @else
+        <img src="{{ asset('images/menus/menunuevo_es.png') }}" alt="Menu del Restaurante" style="display:block;margin:0px auto;">
         <img src="{{ asset('images/menus/grill_es.png') }}" alt="Menu del Restaurante" style="display:block;margin:0px auto;">
         <img src="{{ asset('images/menus/cocodrillos_es.png') }}" alt="Menu del Restaurante" style="display:block;margin:0px auto;">
         <img src="{{ asset('images/menus/bebidas_es.png') }}" alt="Menu del Restaurante" style="display:block;margin:0px auto;">
