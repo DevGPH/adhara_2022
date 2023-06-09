c7d437b68150499feceacd798dd4ab01612eaa51
Juan Pablo Gomez <85317633+DevGPH@users.noreply.github.com>
2023-06-09 19:32:54 +0200

    menu route

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::redirect('/', '/es');
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/menu-adhara',[HomeController::class,'menu'])->name('menu.hotel');
+Route::get('/menu_adhara',[HomeController::class,'menu'])->name('menu.hotel');
 
 Route::prefix('{locale}')->group(function ()
 {
@@ -34,7 +34,7 @@ Route::prefix('{locale}')->group(function ()
     Route::get('/booking',[ReservaController::class,'index'])->name('booking');//se crearon 2 rutas de bookin para resolver el problema de que en el formulario al usar get se busca con la informacion de la consulta anterior
     Route::post('/booking',[ReservaController::class,'index'])->name('bookingP');// y que  con post al cambiar el idioma se recargaba la pagina y como ya no habia parametros en el requets fallaba
     Route::get('/accommodations',[AccommodationsController::class,'accommodations'])->name('accommodations');
-    Route::get('/menu-adhara',[HomeController::class,'menu'])->name('menu.hotel');
+    Route::get('/menu_adhara',[HomeController::class,'menu'])->name('menu.hotel');
     Route::get('/room_service',[HomeController::class,'roomService'])->name('room.service.hotel');
     Route::get('/grupos',[HomeController::class,'grupos'])->name('grupos.hotel');
     Route::get('/gallery',[HomeController::class,'gallery'])->name('gallery');
