1671db6c3f37aa4869e661f169bff78d0f4c61c3
Juan Pablo Gomez <85317633+DevGPH@users.noreply.github.com>
2023-07-13 19:36:12 +0200

    www missing

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -20,7 +20,7 @@ class VerifyCsrfToken extends Middleware
         'http://adharacancun.com/santander/reserve',
         'http://www.adharacancun.com/santander/response',
         'http://www.adharacancun.com/santander/reserve',
-        'http://www.adharacancun.com/santander-mail',
+        'http://adharacancun.com/santander-mail',
         'https://adharacancun.com/test-santander',
         'https://adharacancun.com/santander-test-response',
         'https://adhara-cancun-2022:8890/test-santander'
