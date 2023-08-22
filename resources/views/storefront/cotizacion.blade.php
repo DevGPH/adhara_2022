3a0c7f472a4b460dfc402bdda6f150e8dbbe1431
Juan Pablo Gomez <85317633+DevGPH@users.noreply.github.com>
2023-08-22 22:28:28 +0200

    alert danger

--- resources/views/storefront/cotizacion.blade.php
+++ resources/views/storefront/cotizacion.blade.php
@@ -6,7 +6,7 @@
     <!-- Section Page Title -->
     <div class="section">
         @if(session('error'))
-            <div class="alert alert-warning" role="alert"> {{ session('error') }}
+            <div class="alert alert-danger" role="alert"> {{ session('error') }}
                 <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
