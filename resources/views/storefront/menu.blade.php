ad2fa9cf06cbc90c4da63a14f25368f755ae15f7
Juan Pablo Gomez <85317633+DevGPH@users.noreply.github.com>
2023-09-09 01:18:29 +0200

    restore menus

--- resources/views/storefront/menu.blade.php
+++ resources/views/storefront/menu.blade.php
@@ -5,13 +5,13 @@
 <div class="site-main">
     @if ($lang == 'es')
         <img src="{{ asset('images/menus/ceviche_en.png') }}" alt="Menu del Restaurante" style="display:block;margin:0px auto;">
-        <img src="{{ asset('images/menus/tabemono_en.png') }}" alt="Menu del Restaurante" style="display:block;margin:0px auto;">
-        <img src="{{ asset('images/menus/wano_en.png') }}" alt="Menu del Restaurante" style="display:block;margin:0px auto;">
+        <img src="{{ asset('images/menus/menunuevo_en.png') }}" alt="Menu del Restaurante" style="display:block;margin:0px auto;">
+        <img src="{{ asset('images/menus/cocodrillos_en.png') }}" alt="Menu del Restaurante" style="display:block;margin:0px auto;">
         <img src="{{ asset('images/menus/bebidas_en.png') }}" alt="Menu del Restaurante" style="display:block;margin:0px auto;">
     @else
         <img src="{{ asset('images/menus/ceviche_es.png') }}" alt="Menu del Restaurante" style="display:block;margin:0px auto;">
-        <img src="{{ asset('images/menus/tabemono_es.png') }}" alt="Menu del Restaurante" style="display:block;margin:0px auto;">
-        <img src="{{ asset('images/menus/wano_es.png') }}" alt="Menu del Restaurante" style="display:block;margin:0px auto;">
+        <img src="{{ asset('images/menus/menunuevo_es.png') }}" alt="Menu del Restaurante" style="display:block;margin:0px auto;">
+        <img src="{{ asset('images/menus/cocodrillos_es.png') }}" alt="Menu del Restaurante" style="display:block;margin:0px auto;">
         <img src="{{ asset('images/menus/bebidas_es.png') }}" alt="Menu del Restaurante" style="display:block;margin:0px auto;">
     @endif
 </div>
