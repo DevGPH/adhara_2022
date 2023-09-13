cc76a2988ff2cee5b61344649503a2ff13bb12f5
Juan Pablo Gomez <85317633+DevGPH@users.noreply.github.com>
2023-09-13 16:53:55 +0200

    slide

--- resources/views/_partials/slider.blade.php
+++ resources/views/_partials/slider.blade.php
@@ -3,7 +3,14 @@
 /* No media query since this is the default in Bootstrap */
 
 /* Small devices (tablets, 768px and up) */
-@media (min-width: 768px) {
+.slide-mob{
+    display: none;
+}
+
+.slide-desktop{
+    display: block;
+}
+@media (max-width: 768px) {
     .slide-mob {
         display: block;
     }
@@ -11,21 +18,13 @@
         display: none;
     }
 }
-/* Medium devices (desktops, 992px and up) */
-@media (min-width: 992px) {
-    .slide-mob {
-        display: none;
-    }
-    .slide-desktop {
-        display: block;
-    }
-}
+
 </style>
 <div class="section">
     <div class="widget-slider">
         <div class="wrapper-full">
             <!-- Slider Carousel -->
-            <div class="slide-desktop">
+
                 <div class="widget-carousel owl-carousel owl-theme">
                     @if (App::getLocale() == 'es')
                         <div class="slider-item" data-background="{{ asset('images/sliders/slide_es.png') }}"></div>
@@ -66,51 +65,6 @@
                         </div>
                     </div>
                 </div>
-            </div>
-            <!-- Slider Carousel End -->
-            <!-- Slider Carousel -->
-            <div class="slide-mob">
-                <div class="widget-carousel owl-carousel owl-theme">
-                    @if (App::getLocale() == 'es')
-                        <div class="slider-item" data-background="{{ asset('images/sliders/mobile_es.png') }}"></div>
-                    @else
-                        <div class="slider-item" data-background="{{ asset('images/sliders/mobile_en.png') }}"></div>
-                    @endif
-                    <div class="slider-item" data-background="{{ asset('images/sliders/slider1.png') }}">
-                        <div class="wrapper">
-                            <div class="item-inner">
-                                <!--img src="{{ asset('images/sliders/data.png') }}" alt="" class="slider-special"-->
-                                <h5 style="color:white;">@lang('main.slider.01')</h5>
-                                <h1>${{ $rate }}</h1>
-                                <h2 style="font-family: 'Playfair Display', sans-serif;">@lang('main.slider.02')</h2>
-                                <h5 style="color:white;margin-top:10px;">@lang('main.slider.011')</h5>
-                                <h5 style="color:white;margin-top:10px;">@lang('main.nav.taxes')</h5>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="slider-item" data-background="{{ asset('images/sliders/slide2.png') }}">
-                        <div class="wrapper">
-                            <div class="item-inner">
-                                <!--img src="{{ asset('images/sliders/hotel_1.png') }}" alt="" class="breakpoint-slider"-->
-                                <h5 style="color:white;">@lang('main.slider.03')</h5>
-                                <h1>ONE BED ROOM</h1>
-                                <h2 style="font-family: 'Playfair Display', sans-serif;">@lang('main.slider.04')</h2>
-                                <h5 style="color:white;margin-top:10px;">@lang('main.nav.taxes')</h5>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="slider-item" data-background="{{ asset('images/sliders/slide3.png') }}">
-                        <div class="wrapper">
-                            <div class="item-inner">
-                                <!--img src="{{ asset('images/sliders/club_1.png') }}" alt="" class="breakpoint-slider"-->
-                                <h5 style="color:white;">@lang('main.slider.05')</h5>
-                                <h1>CLUB ESTRELLA</h1>
-                                <h2 style="font-family: 'Playfair Display', sans-serif;">www.clubestrella.mx</h2>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
 
             <!-- Slider Carousel End -->
             <!-- Slider Booking -->
