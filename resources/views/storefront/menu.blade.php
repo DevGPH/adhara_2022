54c6e960b608a17f45016b207834c29b1f63ac0c
BrainLabsDev <admin@brainlabs.tech>
2024-08-17 17:14:58 +0200

    add new sliders menu

--- resources/views/storefront/menu.blade.php
+++ resources/views/storefront/menu.blade.php
@@ -5,12 +5,12 @@
 <div class="site-main">
     @if ($lang == 'es')
         <img src="{{ asset('images/menus/specialty_2024_en.png') }}" alt="Menu del Restaurante" style="display:block;margin:0px auto;">
-        <img src="{{ asset('images/menus/comida_en.png') }}" alt="Menu del Restaurante" style="display:block;margin:0px auto;">
+        <img src="{{ asset('images/menus/entry_en.png') }}" alt="Menu del Restaurante" style="display:block;margin:0px auto;">
         <img src="{{ asset('images/menus/coco_2024_en.png') }}" alt="Menu del Restaurante" style="display:block;margin:0px auto;">
         <img src="{{ asset('images/menus/fluidos_en.png') }}" alt="Menu del Restaurante" style="display:block;margin:0px auto;">
     @else
         <img src="{{ asset('images/menus/specialty_2024_es.png') }}" alt="Menu del Restaurante" style="display:block;margin:0px auto;">
-        <img src="{{ asset('images/menus/comida_es.png') }}" alt="Menu del Restaurante" style="display:block;margin:0px auto;">
+        <img src="{{ asset('images/menus/entry_es.png') }}" alt="Menu del Restaurante" style="display:block;margin:0px auto;">
         <img src="{{ asset('images/menus/coco_2024_es.png') }}" alt="Menu del Restaurante" style="display:block;margin:0px auto;">
         <img src="{{ asset('images/menus/fluidos_es.png') }}" alt="Menu del Restaurante" style="display:block;margin:0px auto;">
     @endif
