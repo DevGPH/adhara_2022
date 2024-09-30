d7867cc0447fbe768467f89482b9746d9de04ec6
BrainLabsDev <admin@brainlabs.tech>
2024-09-30 22:02:24 +0200

    fix: new menu beidas

--- resources/views/storefront/menu.blade.php
+++ resources/views/storefront/menu.blade.php
@@ -7,12 +7,12 @@
         <img src="{{ asset('images/menus/specialty_2024_en.png') }}" alt="Menu del Restaurante" style="display:block;margin:0px auto;">
         <img src="{{ asset('images/menus/entry_en.png') }}" alt="Menu del Restaurante" style="display:block;margin:0px auto;">
         <img src="{{ asset('images/menus/coqui_en.png') }}" alt="Menu del Restaurante" style="display:block;margin:0px auto;">
-        <img src="{{ asset('images/menus/bebidos_en.png') }}" alt="Menu del Restaurante" style="display:block;margin:0px auto;">
+        <img src="{{ asset('images/menus/bebo_en.png') }}" alt="Menu del Restaurante" style="display:block;margin:0px auto;">
     @else
         <img src="{{ asset('images/menus/specialty_2024_es.png') }}" alt="Menu del Restaurante" style="display:block;margin:0px auto;">
         <img src="{{ asset('images/menus/entry_es.png') }}" alt="Menu del Restaurante" style="display:block;margin:0px auto;">
         <img src="{{ asset('images/menus/coqui_es.png') }}" alt="Menu del Restaurante" style="display:block;margin:0px auto;">
-        <img src="{{ asset('images/menus/bebidos_es.png') }}" alt="Menu del Restaurante" style="display:block;margin:0px auto;">
+        <img src="{{ asset('images/menus/bebo_es.png') }}" alt="Menu del Restaurante" style="display:block;margin:0px auto;">
     @endif
 </div>
 
