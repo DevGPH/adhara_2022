77668e6ef255d442a4172d793b7c6167ed415339
BrainLabsDev <admin@brainlabs.tech>
2024-11-17 19:53:26 +0100

    new pops

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -188,9 +188,9 @@
 						<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
 					</div-->
 					@if (App::getLocale() == 'es')
-						<img src="{{ asset('images/sliders/choo_es.png') }}" alt="HotSale">
+						<img src="{{ asset('images/sliders/unpop_es.png') }}" alt="HotSale">
 					@else
-						<img src="{{ asset('images/sliders/kincho_en.png') }}" alt="HotSale">
+						<img src="{{ asset('images/sliders/unpop_en.png') }}" alt="HotSale">
 					@endif
 				</div>
 			</div>
