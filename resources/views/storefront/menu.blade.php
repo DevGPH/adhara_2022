1f67ddc1670c16712cfbdc993be0d1726d5144ee
BrainLabsDev <admin@brainlabs.tech>
2024-12-13 18:22:31 -0600

    new menu

--- resources/views/storefront/menu.blade.php
+++ resources/views/storefront/menu.blade.php
@@ -5,12 +5,12 @@
 <div class="site-main">
     @if ($lang == 'es')
         <img src="{{ asset('images/menus/specialty_2024_en.png') }}" alt="Menu del Restaurante" style="display:block;margin:0px auto;">
-        <img src="{{ asset('images/menus/fck_en.png') }}" alt="Menu del Restaurante" style="display:block;margin:0px auto;">
+        <img src="{{ asset('images/menus/fck_correction_en.png') }}" alt="Menu del Restaurante" style="display:block;margin:0px auto;">
         <img src="{{ asset('images/menus/grillo_en.png') }}" alt="Menu del Restaurante" style="display:block;margin:0px auto;">
         <img src="{{ asset('images/menus/liquido_en.png') }}" alt="Menu del Restaurante" style="display:block;margin:0px auto;">
     @else
         <img src="{{ asset('images/menus/specialty_2024_es.png') }}" alt="Menu del Restaurante" style="display:block;margin:0px auto;">
-        <img src="{{ asset('images/menus/fck_es.png') }}" alt="Menu del Restaurante" style="display:block;margin:0px auto;">
+        <img src="{{ asset('images/menus/fck_correction_es.png') }}" alt="Menu del Restaurante" style="display:block;margin:0px auto;">
         <img src="{{ asset('images/menus/grillo_es.png') }}" alt="Menu del Restaurante" style="display:block;margin:0px auto;">
         <img src="{{ asset('images/menus/liquido_es.png') }}" alt="Menu del Restaurante" style="display:block;margin:0px auto;">
     @endif
