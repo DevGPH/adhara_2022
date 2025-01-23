e19d5bf51bb493ddf70d6feb2a7ecc0cbe976498
BrainLabsDev <admin@brainlabs.tech>
2025-01-23 13:24:41 +0100

    new styles

--- resources/views/storefront/staffmember.blade.php
+++ resources/views/storefront/staffmember.blade.php
@@ -7,15 +7,15 @@
     <div class="section">
         <div class="widget-page-title">
             <div class="widget-background" data-background="{{ asset('images/staff/back.png') }}"></div>
-            <div class="wrapper-inner">
+            <div class="wrapper-inner" style="padding: 0px;">
                 <!-- Title -->
-                <div class="box-staff" style="width: 500px;display:block;margin:auto;">
+                <div class="box-staff" style="width: 400px;display:block;margin:auto;">
                     <img src="{{ asset('images/staff/' . $staff->deparment . '.jpg') }}" alt="{{ $staff->staff_member }}" style="width: 200px;display:block;margin:auto;">
                     <h5 style="width:100%;text-align: center;">@lang('main.keep.touch')</h5>
                     <h1 style="width:100%;text-align: center;">{{ $staff->staff_member }}</h1>
                     <p style="width:100%;text-align: center;margin-top:12px;">{{ $staff->title_es }}</p>
                     <p style="width:100%;text-align: center;margin-top:0px;margin-bottom:20px;">{{ $staff->title_en }}</p>
-                    <ul style="margin: auto;width:fit-content;margin-left: 130px;">
+                    <ul style="margin: auto;width:fit-content;margin-left: 80px;">
                         <li style="margin-bottom: 10px;"><img src="{{ asset('images/staff/t.png') }}" alt="Telephone" style="width: 15px; margin-right:15px;"> Tel. <a href="tel:+52{{  str_replace(' ', '', $staff->telephone)  }}" target="_blank"> {{ $staff->telephone }}</a></li>
                         <li style="margin-bottom: 10px;"><img src="{{ asset('images/staff/c.png') }}" alt="Mobile" style="width: 15px; margin-right:15px;"> Cel. <a href="tel:+52{{  str_replace(' ', '', $staff->mobile)  }}" target="_blank"> {{ $staff->mobile }}</a></li>
                         <li style="margin-bottom: 10px;"><img src="{{ asset('images/staff/m.png') }}" alt="Email" style="width: 15px; margin-right:15px;"> <a href="mailto:{{ $staff->email }}" target="_blank"> {{ $staff->email }}</a></li>
