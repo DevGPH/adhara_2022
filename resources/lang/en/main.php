24f042ae7e0c8fa6862a7b180fb39899469269f2
BrainLabsDev <admin@brainlabs.tech>
2025-08-28 16:46:29 +0200

    feat: add english traduction

--- resources/lang/en/main.php
+++ resources/lang/en/main.php
@@ -617,11 +617,11 @@ return [
     'mayan.subtitle' => 'Your Isla Mujeres Adventure Starts Here!',
     'mayan.text' => "Planning a day trip to the paradise of Isla Mujeres? At Hotel Adhara Cancún, we want your experience to be perfect from the very beginning. As our guest, you'll enjoy a special discount on admission to Mayan Beach, an exclusive beach club located on Playa Norte, considered one of the most beautiful beaches in the world. Spend a pleasant and relaxing day under the sun, with the added benefit that your admission fee is reimbursable towards food and beverage consumption within the club!",
     'mayan.text2' => "For the full terms and conditions of this amazing Mayan Beach benefit, please inquire at our front desk.",
-    'mayan.text3' => '¿Te preguntas cómo llegar a Mayan Beach Club en Isla Mujeres? Es muy sencillo: solo necesitas tomar un ferri desde la Marina de Puerto Juárez.
-        Para que tu viaje sea más fácil, como huésped tienes el beneficio de un traslado gratuito de ida a la marina en nuestra van.
-        <br> - La van sale del hotel todos los días a las 9:00 a.m.
-        <br> - Para asegurar tu lugar, es importante que reserves el servicio con el personal de botones con antelación.
-        <br> - Ten en cuenta que la disponibilidad puede variar.',
+    'mayan.text3' => "Are you wondering how to get to Mayan Beach Club in Isla Mujeres? It's very simple: you just need to take a ferry from Puerto Juárez Marina.
+    To make your trip easier, as a guest you have the benefit of a free one-way transfer to the marina in our van.
+    <br>The van leaves the hotel every day at 9:00 a.m.
+    <br>To secure your spot, it is important to book the service with the bell staff in advance.
+    <br>Please note that availability may vary.",
     'mayan.live' => 'WHATSAPP',
     'mayan.p' => 'Book in Advance',
     'mayan.p2' => 'Your Ferry Transfer',
